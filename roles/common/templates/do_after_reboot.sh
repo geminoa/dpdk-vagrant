396678e386c43d24fccaed430a8fe3d5f0bd604d
geminoa <geminoa@gmail.com>
2016-05-13 00:15:17 +0900

    Add hugepages setting

--- roles/common/templates/do_after_reboot.sh
+++ roles/common/templates/do_after_reboot.sh
@@ -8,6 +8,18 @@ sudo modprobe uio_pci_generic
 echo sudo modprobe vfio-pci
 sudo modprobe vfio-pci
 
+
+# hugepages
+mkdir /mnt/huge
+mount -t hugetlbfs nodev /mnt/huge
+
+# 1024 * 2MB
+echo 1024 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+
+# If on a Numa
+#echo 1024 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+
+
 # setup vars for nic bind.
 script=${RTE_SDK}/tools/dpdk_nic_bind.py
 opt="--bind=uio_pci_generic"
